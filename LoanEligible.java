0398309c8fdabcf80a56380afc7d0273ae20bd1a
ssnehajachar-cmyk <ssnehajachar@gmail.com>
2026-03-26 11:25:38 +0530

    Upload LoanEligible

---
+++ LoanEligible.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LoanEligible {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age;
+        float salary;
+        System.out.println("Enter age: " );
+        age=sc.nextInt();
+            System.out.println("Enter salary: ");
+        salary=sc.nextInt();
+        if(age>=20 && salary>=21000)
+
+             System.out.println("Eligible to take loan");
+        else
+            System.out.println("Not eligible");
+        sc.close();
+        
+    }
+            
+        
+}
+
+
+    
+

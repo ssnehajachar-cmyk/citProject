dedd7f3ac322d53fbcc109245009236eeb40e68b
ssnehajachar-cmyk <ssnehajachar@gmail.com>
2026-03-26 14:00:42 +0530

    upload jumpstatement.java

--- JumpStatement.java
+++ JumpStatement.java
@@ -2,7 +2,8 @@ public class JumpStatement {
     public static void main(String[] args) {
         for(int i=1;i<10;i++){
             if(i==5)
-                break;
+               // break;
+            continue;
             System.out.println("value is: "+i);
         }
     }

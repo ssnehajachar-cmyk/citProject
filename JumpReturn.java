0c26c9c46935c55836145e2bba284dc0067e7e81
ssnehajachar-cmyk <ssnehajachar@gmail.com>
2026-03-26 14:52:59 +0530

    update primeornot

---
+++ JumpReturn.java
@@ -0,0 +1,12 @@
+public class JumpReturn {
+    public static void main(String[] args) {
+        
+        System.out.println("before return statement");
+        return;
+        //this is not allowed and gives error
+       // System.out.println("Error after return statement");
+
+
+    }
+    
+}

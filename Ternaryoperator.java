327da16955903bc4acf0d3c906d1b681b7187d7a
ssnehajachar-cmyk <ssnehajachar@gmail.com>
2026-03-26 10:13:57 +0530

    Updated constrol structures

--- Ternaryoperator.java
+++ Ternaryoperator.java
@@ -1,9 +1,8 @@
 public class Ternaryoperator {
-    public static void main(String[] args) {
+    public static void main(String[] args) { git config --global user.name "Your Name"
         int age=18;
         String result=(age>=18)?"Eligible to vote":"Not Eligible";
         System.out.println(result);
     }
     
 }
-""

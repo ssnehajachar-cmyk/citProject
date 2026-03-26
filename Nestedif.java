ac895448b9a665acb835ba5a9196b8d05d945d50
ssnehajachar-cmyk <ssnehajachar@gmail.com>
2026-03-26 12:10:10 +0530

    upload nestefif.java

--- Nestedif.java
+++ Nestedif.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Nestedif {
     public static void main(String[] args) {
-        int age=65;
+        //int age = 65;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the age");
+        int age =sc.nextInt();
+        if (age>=18){
+
+        };
+    
         if(age>=18){
             if(age>=65){
                 System.out.println("Eligible to avail government fund");

7c90f8e4ce7c5292ec13ea35d950cfd4c480e2d1
ssnehajachar-cmyk <ssnehajachar@gmail.com>
2026-03-27 12:20:46 +0530

    upload reversenumber

--- firstdigit.java
+++ firstdigit.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
-public class lastdigit {
+public class firstdigit {
     public static void main(String[] args){
         Scanner sc = new Scanner (System.in);
         System.out.print("enter a number:");
         int num=sc.nextInt();
-        int lastdigit=num%10;
-        System.out.println("output:"+lastdigit);
+        while(num>=10){
+            num=num/10;
+        }
+        
+        System.out.println("firstdigit is:" +num);
         
     }
 }
---
+++ swap.java
@@ -0,0 +1,12 @@
+public class swap {
+    public static void main(String[] args) {
+        int a=5,b=10;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+    }
+    
+}
